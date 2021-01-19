c860d4258b2f3211c19d3aa399c0d95dd1aa721c
Erick <Luzivotto.erick@gmail.com>
2021-01-18 21:20:59 -0300

    Adding Scripts
    basic SpaceShip Movement

---
+++ Assets/Scripts/Classe_NavePadrão.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classe_NavePadrão 
+{
+    [SerializeField]
+    float MultVelocidade, VelocidadeMaximaNave,MultTorque,VelocidadeDesaceleração;
+
+    [SerializeField]
+    Rigidbody NaveBody;
+
+
+    //Funções Gets e Sets
+
+
+    //pegando o rigidbody
+     public Rigidbody GetRigidbodyNave()
+    {
+        return NaveBody;
+    }
+    //settando o rigidbody
+    public void SetNaveRigidbody(Rigidbody Nave)
+    {
+        NaveBody = Nave;
+    }
+
+
+    //pegando o torque da nave
+    public float GetTorquenave()
+    {
+        return MultTorque;
+    }
+    //Mudando o torque da nave
+    public void SetNaveTorque(float novoTorque)
+    {
+        MultTorque = novoTorque;
+    }
+    //Aumentando/Diminuindo o torque da nave
+    public void AddNaveTorque(float NovoTorque)
+    {
+        MultTorque += NovoTorque;
+    }
+
+
+
+
+    //pegando a desaceleração da nave
+    public float GetDesaceleração()
+    {
+        return VelocidadeDesaceleração;
+    }
+    //Mudando a desaceleração da nave
+    public void SetDesaceleração(float novaDesaceleração)
+    {
+        VelocidadeDesaceleração = novaDesaceleração;
+    }
+    //Aumentando/Diminuindo a desaceleração da nave
+    public void AddDesaceleração(float novaDesaceleração)
+    {
+        VelocidadeDesaceleração += novaDesaceleração;
+    }
+
+
+
+
+    //retorna o multiplicador da velocidade
+    public float GetMultVelocidade()
+    {
+        return MultVelocidade;
+    }
+
+    //retorna velocidade maxima da nave
+    public float GetMaxVelocidade()
+    {
+        return VelocidadeMaximaNave;
+    }
+
+    //muda o valor do multiplicador de velocidade
+    public void SetMultVelocidade(float novoMultVelocidade)
+    {
+        MultVelocidade = novoMultVelocidade;
+    }
+
+    //muda o valor da velocidade maxima
+    public void SetMaxVelocidade(float novoVelocidademax)
+    {
+        VelocidadeMaximaNave = novoVelocidademax;
+    }
+
+    //adiciona/subtrai o valor do multiplicador de velocidade
+    public void AddMultVelocidade(float AddMultVelocidade)
+    {
+        MultVelocidade += AddMultVelocidade;
+    }
+
+    //adiciona/subtrai o valor da velocidade maxima
+    public void AddVelocidadeMax(float AddVelocidadeMax)
+    {
+        VelocidadeMaximaNave += AddVelocidadeMax;
+    }
+
+
+}
---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public Rigidbody Player;
+    public Camera Player1Camera;
+
+    Classe_NavePlayer1 Navezinha1;
+
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.GetComponent<Rigidbody>();
+        Navezinha1 = new Classe_NavePlayer1(10,250,Player,180,25);
+        Player1Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        velocidade = Player.velocity.magnitude;
+
+
+
+        Player1Camera.transform.position =
+            new Vector3(
+                transform.position.x,
+                transform.position.y + 13,
+                transform.position.z
+                );
+
+
+            if (Input.GetButton("Player1Foward"))
+            {
+                Player.AddForce(Player.transform.forward * Navezinha1.GetMultVelocidade() * Time.deltaTime, ForceMode.Acceleration);
+                    if(Player.velocity.magnitude > Navezinha1.GetMaxVelocidade())
+                    {
+                        Player.velocity = Player.velocity.normalized * Navezinha1.GetMaxVelocidade();
+                    }
+            }
+
+        if (Input.GetButton("Player1Backward"))
+        {
+            Player.AddForce(-Player.transform.forward * Navezinha1.GetMultVelocidade() * Time.deltaTime, ForceMode.Acceleration);
+            if (Player.velocity.magnitude > Navezinha1.GetMaxVelocidade())
+            {
+                Player.velocity = Player.velocity.normalized * Navezinha1.GetMaxVelocidade();
+            }
+        }
+
+
+        if (Input.GetButton("Player1Left"))
+        {
+            Player.AddTorque(-Player.transform.up * Navezinha1.GetTorquenave() * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+        if (Input.GetButton("Player1Right"))
+        {
+            Player.AddTorque(Player.transform.up * Navezinha1.GetTorquenave() * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+
+
+
+
+    }
+}
